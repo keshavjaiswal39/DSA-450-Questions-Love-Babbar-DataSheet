cafa97c9c46b60affa81b45b241d65261e3f26c6
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-03-16 18:56:52 +0530

    Find the two non-repeating elements in an array of repeating elements new

--- Bit Manipulation/Find the two non-repeating elements in an array/Find the two non-repeating elements in an array.cpp
+++ Bit Manipulation/Find the two non-repeating elements in an array/Find the two non-repeating elements in an array.cpp
@@ -1,40 +1,38 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main()
-{
-	int n;
-	cin>>n;
-	int a[100005];
-	int xor_=0;
-	for(int i=0;i<n;i++)
-	{
-		cin>>a[i];
-		xor_=xor_^a[i];     // we get the total xor of the value
-	}
-	
-	int temp=xor_;
-	int pos=0;
-	while((temp&1)!=1)
-	{
-		pos++;
-		temp=temp>>1;      // here we get the position of the last set bit in the final answer
-	}
-	
-	int mask=(1<<pos);     // creating a mask with the set bit 1
-	
-	int x=0;     // the elements which are occured only once
-	int y=0;    // the elements which are occured only once
-	
-	for(int i=0;i<n;i++)
-	{
-		if((a[i]&mask)>0)
-		{
-			x=x^a[i];
-		}
-	}
-	y=xor_^x;
-	
-	cout<<min(x,y)<<" "<<max(x,y);
-	
-}
+vector<int> singleNumber(vector<int> nums) 
+    {
+        // Code here.
+        vector<int> v;
+        int xor_=0;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            xor_=xor_^nums[i];
+        }
+        
+        int temp=xor_;
+        int pos=0;
+        
+        while((temp&1)!=1)
+        {
+            pos++;
+            temp=temp>>1;
+        }
+        int x=0;
+        int y=0;
+        
+        int mask=1<<pos;
+        
+        for(int i=0;i<nums.size();i++)
+	    {
+		    if((nums[i]&mask)>0)
+	    	{
+		    	x=x^nums[i];
+		    }
+    	}
+	    y=xor_^x;
+	    
+	    v.push_back(min(x,y));
+	    v.push_back(max(x,y));
+	    
+	    return v;
+    }
