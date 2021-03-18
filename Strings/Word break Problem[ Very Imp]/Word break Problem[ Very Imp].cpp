7e0eb3299a323ce9c4c4040a40de4688b8d774b2
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-03-18 13:54:36 +0530

    Word break Problem[ Very Imp]

---
+++ Strings/Word break Problem[ Very Imp]/Word break Problem[ Very Imp].cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+unordered_map<string,int> dp;
+
+int solve(string s,vector<string> &b)
+{
+	int len=s.size();
+	
+	if(len==0)
+	{
+		return 1;
+	}
+	
+	if(dp[s]!=0)
+	{
+		return dp[s];
+	}
+	
+	for(int i=1;i<=len;i++)
+	{
+		int flag=0;
+		string search=s.substr(0,i);
+		
+		for(int j=0;j<b.size();j++)
+		{
+			if(search.compare(b[j])==0)
+			{
+				flag=1;
+				break;
+			}
+		}
+		
+		if(flag==1 and solve(s.substr(i,len-i),b)==1)
+		{
+			return dp[s]=1;
+		}
+	}
+	return dp[s]=-1;
+}
+
+int wordBreak(string A,vector<string> &B)
+{
+	int x=solve(A,B);
+	
+	if(x==1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
