cf8aec305e20ec21747b78af652dcb25d6e799eb
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-06-16 21:27:11 +0530

    Merge “K” Sorted Linked Lists [V.IMP]

---
+++ Array/Count triplets in a sorted DLL whose/Count triplets in a sorted DLL whose sum naive approach.cpp
@@ -0,0 +1,19 @@
+int countTriplets(struct Node* head, int x)
+{
+    struct Node* ptr1, *ptr2, *ptr3;
+    int count = 0;
+ 
+    // generate all possible triplets
+    for (ptr1 = head; ptr1 != NULL; ptr1 = ptr1->next)
+        for (ptr2 = ptr1->next; ptr2 != NULL; ptr2 = ptr2->next)
+            for (ptr3 = ptr2->next; ptr3 != NULL; ptr3 = ptr3->next)
+ 
+                // if elements in the current triplet sum up to 'x'
+                if ((ptr1->data + ptr2->data + ptr3->data) == x)
+ 
+                    // increment count
+                    count++;
+ 
+    // required count of triplets
+    return count;
+}
---
+++ Array/Count triplets in a sorted DLL whose/Count triplets in a sorted DLL whose sum using two pointer approach.cpp
@@ -0,0 +1,70 @@
+// function to count pairs whose sum equal to given 'value'
+int countPairs(struct Node* first, struct Node* second, int value)
+{
+    int count = 0;
+ 
+    // The loop terminates when either of two pointers
+    // become NULL, or they cross each other (second->next
+    // == first), or they become same (first == second)
+    while (first != NULL && second != NULL &&
+           first != second && second->next != first) {
+ 
+        // pair found
+        if ((first->data + second->data) == value) {
+ 
+            // increment count
+            count++;
+ 
+            // move first in forward direction
+            first = first->next;
+ 
+            // move second in backward direction
+            second = second->prev;
+        }
+ 
+        // if sum is greater than 'value'
+        // move second in backward direction
+        else if ((first->data + second->data) > value)
+            second = second->prev;
+ 
+        // else move first in forward direction
+        else
+            first = first->next;
+    }
+ 
+    // required count of pairs
+    return count;
+}
+
+// function to count triplets in a sorted doubly linked list
+// whose sum is equal to a given value 'x'
+int countTriplets(struct Node* head, int x)
+{
+    // if list is empty
+    if (head == NULL)
+        return 0;
+ 
+    struct Node* current, *first, *last;
+    int count = 0;
+ 
+    // get pointer to the last node of
+    // the doubly linked list
+    last = head;
+    while (last->next != NULL)
+        last = last->next;
+ 
+    // traversing the doubly linked list
+    for (current = head; current != NULL; current = current->next) {
+ 
+        // for each current node
+        first = current->next;
+ 
+        // count pairs with sum(x - current->data) in the range
+        // first to last and add it to the 'count' of triplets
+        count += countPairs(first, last, x - current->data);
+    }
+ 
+    // required count of triplets
+    return count;
+}
+ 
