21a91a7f3534b6072fe537b2fc5372cc91792a2a
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-06-16 22:45:37 +0530

    Check if a Binary Tree is Heap

---
+++ Heap/Check if a Binary Tree is Heap/Check if a Binary Tree is Heap.cpp
@@ -0,0 +1,46 @@
+class Solution {
+  public:
+    bool isHeap(struct Node * root)
+    {
+        // code here
+        queue<Node*> q;
+        q.push(root);
+        
+        bool nullish = false;
+        
+        while (!q.empty()) 
+        {
+            Node* temp = q.front();
+            q.pop();
+            
+            if (temp->left) 
+            {
+                if (nullish || temp->left->data >= temp->data) 
+                {
+                    return false;
+                }
+                q.push(temp->left);
+            }
+            
+            else 
+            {
+                nullish = true;
+            }
+            
+            if (temp->right)
+            {
+                if (nullish || temp->right->data >= temp->data) 
+                {
+                    return false;
+                }
+                q.push(temp->right);
+            }
+            
+            else
+            {
+                nullish = true;
+            }
+        }
+        return true;
+    }
+};
