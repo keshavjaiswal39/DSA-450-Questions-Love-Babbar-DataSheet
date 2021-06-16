c77af07b589913b2abe1d4baef86b5e8e6eb6ca8
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-06-16 14:03:58 +0530

    Reverse a Doubly Linked list.

---
+++ LinkedList/Reverse a Doubly Linked list/Reverse a Doubly Linked list..cpp
@@ -0,0 +1,14 @@
+if(head == NULL || head->next == NULL)
+    return head;
+    
+    Node *prev = NULL, *curr = head;
+    
+    while(curr != NULL){
+        prev = curr->prev;
+        curr->prev = curr->next;
+        curr->next = prev;
+        
+        curr = curr->prev;
+    }
+    
+    return prev->prev;
