980ba322c2234fe3b4db9078bb98432aa708149c
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-06-20 13:51:01 +0530

    Evaluation of Postfix expression

---
+++ Stacks and Queues/Evaluation of Postfix expression/Evaluation of Postfix expression.cpp
@@ -0,0 +1,35 @@
+// The main function that returns value of a given postfix expression
+int evaluatePostfix(char* exp)
+{
+    // Create a stack of capacity equal to expression size
+    struct Stack* stack = createStack(strlen(exp));
+    int i;
+  
+    // See if stack was created successfully
+    if (!stack) return -1;
+  
+    // Scan all characters one by one
+    for (i = 0; exp[i]; ++i)
+    {
+        // If the scanned character is an operand (number here),
+        // push it to the stack.
+        if (isdigit(exp[i]))
+            push(stack, exp[i] - '0');
+  
+        // If the scanned character is an operator, pop two
+        // elements from stack apply the operator
+        else
+        {
+            int val1 = pop(stack);
+            int val2 = pop(stack);
+            switch (exp[i])
+            {
+            case '+': push(stack, val2 + val1); break;
+            case '-': push(stack, val2 - val1); break;
+            case '*': push(stack, val2 * val1); break;
+            case '/': push(stack, val2/val1); break;
+            }
+        }
+    }
+    return pop(stack);
+} 
